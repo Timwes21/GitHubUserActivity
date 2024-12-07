a15a8b12a86cec9b378adc27b731f899560706ff
Timothy Wesley <timothy.wesley99@yahoo.com>
2024-12-07 03:49:25 -0500

    Grouped similar activity together

--- app.py
+++ app.py
@@ -1,40 +1,55 @@
 import requests
-import json
-github = "https://api.github.com/users/Erik-Cupsa/events"
-json_file = "events.json"
+
+account = input("Who's activity would you like to follow: ")
+github = f"https://api.github.com/users/{account}/events"
 
 page = requests.get(github).json()
-
-
-print(page)   
-
-
-
-print("recent activity:")
-def commits():
-    pass
     
-    
-def sparse(data):
-    if data['type'] == "CreateEvent":
-        print(f"Create Event in {data['repo']['name']}")
-    elif data['type'] == "PushEvent":
-        print(f"Pushed Commit in {data['repo']['name']}")
-    elif data['type'] == "WatchEvent":
-        print(f"Starred {data['repo']['name']}")
-    elif data['type'] == "PullRequestEvent":
-        print(f"Pull Request in {data['repo']['name']}")
-    elif data['type'] == "MemberEvent":
-        print(f"Updated a Collaboration in {data['repo']['name']}")
-    elif data['type'] == "PublicEvent":
-        print(f"{data['repo']['name']} was made public")
+def sparse(type, name, num):
+    if type == "CreateEvent":
+        print(f"- {num} Create Event(s) in {name}")
+    elif type == "PushEvent":
+        print(f"- Pushed {num} Commit(s) in {name}")
+    elif type == "WatchEvent":
+        print(f"- Starred {name}")
+    elif type == "PullRequestEvent":
+        print(f"- {num} Pull Request(s) in {name}")
+    elif type == "MemberEvent":
+        print(f"- Updated a Collaboration in {name}")
+    elif type == "PublicEvent":
+        print(f"- {name} was made public")
     else:
-        print(data["type"])
+        print(type)
+
+
+n = 0
+list_of_data = []
+    
+    
+    
+def sparser(data, n):
+    if n > 0:
+        if data['type'] == list_of_data[0]['type'] and data['repo']['name'] == list_of_data[0]['repo']['name']:
+            list_of_data.append(data)
+            n += 1
+        else:
+            sparse(list_of_data[0]['type'], list_of_data[0]['repo']['name'], len(list_of_data))
+            list_of_data.clear()
+            list_of_data.append(data)
+            n = 0
+    else:
+        list_of_data.append(data)
+        n += 1
         
+    return n
+        
+    
+        
+    
 for i in page:
-    sparse(i)
+    n = sparser(i, n)
     
-    
+
 
 
 
