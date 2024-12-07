e1b16a2d627beea838eed3ab39b336083c2aafae
Timothy Wesley <timothy.wesley99@yahoo.com>
2024-12-07 14:59:51 -0500

    added omments and updated the input message

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import requests
 
-account = input("Who's activity would you like to follow: ")
+account = input("GitHub-Activity ")
 github = f"https://api.github.com/users/{account}/events"
 
-page = requests.get(github).json()
+page = requests.get(github)
+
+while page.status_code != 200:
+    print("status code not 200")  # since it is a command line, I wanted an appropriate response
+
+page = page.json() # turns the page into json format
     
-def sparse(type, name, num):
+def sparse(type, name, num): # funcction to measure the size, type and name, the evaluate the response needed
     if type == "CreateEvent":
         print(f"- {num} Create Event(s) in {name}")
     elif type == "PushEvent":
@@ -23,19 +28,21 @@ def sparse(type, name, num):
 
 
 n = 0
-list_of_data = []
+list_of_data = [] # Initialize a list to store duplicates
     
     
     
 def sparser(data, n):
-    if n > 0:
+    if n > 0: # intead of using the length of duplicates found, use n to stay in the if statement when the list is cleared
+        # if there is a duplicate store it in the list
         if data['type'] == list_of_data[0]['type'] and data['repo']['name'] == list_of_data[0]['repo']['name']:
             list_of_data.append(data)
             n += 1
         else:
+            # if the next one is not a duplicate print the list of duplicates
             sparse(list_of_data[0]['type'], list_of_data[0]['repo']['name'], len(list_of_data))
-            list_of_data.clear()
-            list_of_data.append(data)
+            list_of_data.clear() # after the duplicates are displayed clear the list
+            list_of_data.append(data) # append data to list to check for more duplicates
             n = 0
     else:
         list_of_data.append(data)
@@ -45,7 +52,7 @@ def sparser(data, n):
         
     
         
-    
+# for loop to check the data in the page
 for i in page:
     n = sparser(i, n)
     
